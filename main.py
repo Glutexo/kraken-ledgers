f2bda951bd018558622a8a1e5202e8d697c571b0
Glutexo <glutexo@icloud.com>
2024-03-28 12:52:38 +0100

    Sum trades

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ def _process_entry(
     old_deposits,
     old_deposit_fees,
     old_withdrawals,
-    old_withdrawal_fees
+    old_withdrawal_fees,
+    old_trades,
+    old_trade_fees,
 ):
     new_deposits = totaldict(**old_deposits)
     new_deposit_fees = totaldict(**old_deposit_fees)
     new_withdrawals = totaldict(**old_withdrawals)
     new_withdrawal_fees = totaldict(**old_withdrawal_fees)
+    new_trades = totaldict(**old_trades)
+    new_trade_fees = totaldict(**old_trade_fees)
 
     if entry["type"] == "deposit":
         new_deposits[entry["asset"]] += Decimal(entry["amount"])
@@ -26,10 +30,20 @@ def _process_entry(
     elif entry["type"] == "withdrawal":
         new_withdrawals[entry["asset"]] += Decimal(entry["amount"])
         new_withdrawal_fees[entry["asset"]] += Decimal(entry["fee"])
+    elif entry["type"] == "trade":
+        new_trades[entry["asset"]] += Decimal(entry["amount"])
+        new_trade_fees[entry["asset"]] += Decimal(entry["fee"])
     else:
         raise ValueError(f"Unknown entry type: {entry['type']}")
 
-    return new_deposits, new_deposit_fees, new_withdrawals, new_withdrawal_fees
+    return (
+        new_deposits,
+        new_deposit_fees,
+        new_withdrawals,
+        new_withdrawal_fees,
+        new_trades,
+        new_trade_fees,
+    )
 
 
 def _format_totals(deposits):
@@ -51,16 +65,26 @@ def main(input_path):
         deposit_fees = totaldict()
         withdrawals = totaldict()
         withdrawal_fees = totaldict()
+        trades = totaldict()
+        trade_fees = totaldict()
         for entry in _read_csv(input_file):
             try:
-                deposits, deposit_fees, withdrawals, withdrawal_fees =\
-                    _process_entry(
-                        entry,
-                        deposits,
-                        deposit_fees,
-                        withdrawals,
-                        withdrawal_fees
-                    )
+                (
+                    deposits,
+                    deposit_fees,
+                    withdrawals,
+                    withdrawal_fees,
+                    trades,
+                    trade_fees,
+                ) = _process_entry(
+                    entry,
+                    deposits,
+                    deposit_fees,
+                    withdrawals,
+                    withdrawal_fees,
+                    trades,
+                    trade_fees,
+                )
             except ValueError:
                 unprocessed.append(entry)
 
@@ -72,7 +96,9 @@ def main(input_path):
         (deposits, "deposits"),
         (deposit_fees, "deposit_fees"),
         (withdrawals, "withdrawals"),
-        (withdrawal_fees, "withdrawal_fees")
+        (withdrawal_fees, "withdrawal_fees"),
+        (trades, "trades"),
+        (trade_fees, "trade_fees"),
     ]:
         print(f"Total {description}:")
         for line in _format_totals(totals):
