52e38226456a64e69687119232d30f0c2775225c
Glutexo <glutexo@icloud.com>
2024-03-28 14:34:23 +0100

    Factor out processing functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from csv import DictReader
 from collections import defaultdict
 from collections import namedtuple
+from copy import copy
 from decimal import Decimal
 from sys import argv
 
@@ -32,45 +33,85 @@ def totaldict(**kwargs):
     return defaultdict(AmountWithFee, **kwargs)
 
 
-def _process_entry(entry, old_deposits, old_withdrawals, old_trades):
-    new_deposits = totaldict(**old_deposits)
-    new_withdrawals = totaldict(**old_withdrawals)
-    new_trades = totaldict(**old_trades)
+class Totals(namedtuple(
+    "Totals",
+    ("deposits", "withdrawals", "trades"),
+    defaults=(totaldict(), totaldict(), totaldict())
+)):
+    def __copy__(self):
+        return Totals(
+            totaldict(**self.deposits),
+            totaldict(**self.withdrawals),
+            totaldict(**self.trades),
+        )
+
+
+def _process_deposit(entry, old_totals):
+    new_amount = Decimal(entry["amount"])
+    if new_amount < zero:
+        raise EntryValueError(
+            f"Negative deposit amount: {new_amount}"
+        )
+
+    new_totals = copy(old_totals)
+    old_deposit = new_totals.deposits[entry["asset"]]
+
+    amount = old_deposit.amount + new_amount
+    fee = old_deposit.fee - Decimal(entry["fee"])
+    new_totals.deposits[entry["asset"]] = AmountWithFee(amount, fee)
+
+    return new_totals
+
+
+def _process_withdrawal(entry, old_totals):
+    new_amount = Decimal(entry["amount"])
+    if new_amount > zero:
+        raise EntryValueError(
+            f"Positive withdrawal amount: {new_amount}"
+        )
+
+    new_totals = copy(old_totals)
+    old_withdrawal = new_totals.withdrawals[entry["asset"]]
+
+    amount = old_withdrawal.amount - new_amount
+    fee = old_withdrawal.fee - Decimal(entry["fee"])
+    new_totals.withdrawals[entry["asset"]] = AmountWithFee(amount, fee)
+
+    return new_totals
+
+
+def _process_trade(entry, old_totals):
+    new_totals = copy(old_totals)
+    old_trade = new_totals.trades[entry["asset"]]
+
+    amount = old_trade.amount + Decimal(entry["amount"])
+    fee = old_trade.fee - Decimal(entry["fee"])
+    new_totals.trades[entry["asset"]] = AmountWithFee(amount, fee)
+
+    return new_totals
+
+
+entry_type_processors = {
+    "deposit": _process_deposit,
+    "withdrawal": _process_withdrawal,
+    "trade": _process_trade,
+}
+
+
+def _process_entry(entry, old_totals):
+    new_totals = Totals()
 
     new_fee = Decimal(entry["fee"])
     if new_fee > 0:
         raise EntryValueError(f"Positive fee amount: {new_fee}")
 
-    if entry["type"] == "deposit":
-        new_amount = Decimal(entry["amount"])
-        if new_amount < zero:
-            raise EntryValueError(
-                f"Negative deposit amount: {new_amount}"
-            )
-        old_deposit = new_deposits[entry["asset"]]
-        amount = old_deposit.amount + new_amount
-        fee = old_deposit.fee - Decimal(entry["fee"])
-        new_deposits[entry["asset"]] = AmountWithFee(amount, fee)
-    elif entry["type"] == "withdrawal":
-        new_amount = Decimal(entry["amount"])
-        if new_amount > zero:
-            raise EntryValueError(
-                f"Positive withdrawal amount: {new_amount}"
-            )
-
-        old_withdrawal = new_withdrawals[entry["asset"]]
-        amount = old_withdrawal.amount - new_amount
-        fee = old_withdrawal.fee - Decimal(entry["fee"])
-        new_withdrawals[entry["asset"]] = AmountWithFee(amount, fee)
-    elif entry["type"] == "trade":
-        old_trade = new_trades[entry["asset"]]
-        amount = old_trade.amount + Decimal(entry["amount"])
-        fee = old_trade.fee - Decimal(entry["fee"])
-        new_trades[entry["asset"]] = AmountWithFee(amount, fee)
-    else:
+    try:
+        processor = entry_type_processors[entry["type"]]
+    except KeyError:
         raise EntryTypeError(f"Unknown entry type: {entry['type']}")
 
-    return new_deposits, new_withdrawals, new_trades
+    new_totals = processor(entry, old_totals)
+    return new_totals
 
 
 def _format_totals(deposits):
@@ -91,14 +132,10 @@ def _read_csv(file):
 def main(input_path):
     with open(input_path, "r") as input_file:
         unprocessed = []
-        deposits = totaldict()
-        withdrawals = totaldict()
-        trades = totaldict()
+        totals = Totals()
         for entry in _read_csv(input_file):
             try:
-                deposits, withdrawals, trades = _process_entry(
-                    entry, deposits, withdrawals, trades
-                )
+                totals = _process_entry(entry, totals)
             except EntryTypeError:
                 unprocessed.append(entry)
 
@@ -107,9 +144,9 @@ def main(input_path):
         print()
 
     for totals, description in [
-        (deposits, "deposits"),
-        (withdrawals, "withdrawals"),
-        (trades, "trades"),
+        (totals.deposits, "deposits"),
+        (totals.withdrawals, "withdrawals"),
+        (totals.trades, "trades"),
     ]:
         print(f"Total {description}:")
         for line in _format_totals(totals):
