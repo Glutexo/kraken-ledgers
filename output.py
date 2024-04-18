7707bc7c05e5e07ebb10ae6ba913fe55ef44c485
Glutexo <glutexo@icloud.com>
2024-04-18 17:28:12 +0200

    Remove lambda building

--- output.py
+++ output.py
@@ -1,48 +1,40 @@
+from collections import namedtuple
 from csv import DictWriter
 from itertools import product
 
 from total import Trade
 
+FIELD_FUNCTIONS = {
+    "asset": lambda key, value: key,
+    "amount": lambda key, value: value.amount,
+    "fee": lambda key, value: value.fee,
+}
 
-def make_field_functions():
-    entry_field_functions = {
-        "asset": lambda key, value: key,
-        "amount": lambda key, value: value.amount,
-        "fee": lambda key, value: value.fee,
-    }
-
-    def make_product_function(trade_fld, entry_fld):
-        def product_func(trade_total):
-            base_field_function = entry_field_functions[entry_fld]
-
-            trade_total_key, trade_total_value = trade_total
-            key = getattr(trade_total_key, trade_fld)
-            value = getattr(trade_total_value, trade_fld)
-
-            return base_field_function(key, value)
-
-        return product_func
-
-    product_functions = {}
-    fields_product = product(Trade._fields, entry_field_functions.keys())
-    for trade_field, entry_field in fields_product:
-        product_field_name = f"{trade_field}_{entry_field}"
-        product_function = make_product_function(trade_field, entry_field)
-        product_functions[product_field_name] = product_function
-
-    return product_functions
+Field = namedtuple("Field", ["trade_field", "trade_total_field"])
 
 
-FIELD_FUNCTIONS = make_field_functions()
-del make_field_functions
+def populate_fields():
+    fields = {}
+    fields_product = product(Trade._fields, FIELD_FUNCTIONS.keys())
+
+    for trade_field, trade_total_field in fields_product:
+        key = "_".join([trade_field, trade_total_field])
+        value = Field(trade_field, trade_total_field)
+        fields[key] = value
+
+    return fields
+
+
+FIELDS = populate_fields()
+del populate_fields
 
 
 class TradeTotalsWriter:
     def __init__(self, file):
-        fieldnames = FIELD_FUNCTIONS.keys()
+        field_names = FIELDS.keys()
         self.writer = DictWriter(
             file,
-            fieldnames=fieldnames,
+            fieldnames=field_names,
             delimiter=",",
             lineterminator="\n",
             doublequote=True,
@@ -51,7 +43,10 @@ class TradeTotalsWriter:
 
     def write(self, trade_total):
         row = {}
-        for field, function in FIELD_FUNCTIONS.items():
-            row[field] = function(trade_total)
+        for row_field, (trade_field, trade_total_field) in FIELDS.items():
+            trade_total_key, trade_total_value = trade_total
+            key = getattr(trade_total_key, trade_field)
+            value = getattr(trade_total_value, trade_field)
+            row[row_field] = FIELD_FUNCTIONS[trade_total_field](key, value)
 
         self.writer.writerow(row)
