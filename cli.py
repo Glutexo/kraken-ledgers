f0fba6d995a282a5ab8a4521a94115b6a357ed0e
Glutexo <glutexo@icloud.com>
2024-04-19 07:39:17 +0200

    Replace custom CLI with argparse

--- cli.py
+++ cli.py
@@ -1,43 +1,16 @@
-from enum import IntEnum
-from sys import argv
-from sys import stderr
+from argparse import ArgumentParser
+from argparse import FileType
 
 __all__ = ["main"]
 
-USAGE = "Usage: main.py <filename>"
-
-ExitCode = IntEnum(
-    "ExitCode",
-    ["ok", "invalid_usage", "file_not_found", "unsufficent_permissions"],
-    start=0,
-)
-
-
-def print_error(*args):
-    print(*args, file=stderr)
-
 
 def main(callback):
-    try:
-        filename = argv[1]
-    except IndexError:
-        print_error("No file specified.")
-        print_error(USAGE)
-        exit(ExitCode.invalid_usage)
+    parser = ArgumentParser(description="Parse Kraken ledgers file.")
+    parser.add_argument("--ledgers", type=FileType("r"), default="ledgers.csv")
+    args = parser.parse_args()
 
-    try:
-        file = open(filename, "r")
-    except FileNotFoundError:
-        print_error("File not found.")
-        exit(ExitCode.file_not_found)
-    except PermissionError:
-        print_error("Unsufficent permissions.")
-        exit(ExitCode.unsufficent_permissions)
-
-    callback(file)
-    file.close()
-
-    exit(ExitCode.ok)
+    callback(args.ledgers)
+    args.ledgers.close()
 
 
 if __name__ == "__main__":
